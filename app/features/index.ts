d58779b81507870c44648a6d916bd4a1f7da92c6
Malloy <lefehanims@gmail.com>
2026-03-19 15:02:57 -0600

    remove stats page

--- app/features/index.ts
+++ app/features/index.ts
@@ -3,5 +3,4 @@ export { Downloads } from "./downloads";
 export { Engines } from "./engines";
 export { Library } from "./library";
 export { Settings } from "./settings";
-export { Stats } from "./stats";
 export { Updates } from "./updates";
