c7aa91b56aa30be7edfdbb6189314b6d58ce7cb8
ImMalloy <lefehanims@gmail.com>
2026-04-13 20:36:49 -0600

    trim mods exports

--- app/features/mods/index.ts
+++ app/features/mods/index.ts
@@ -1,13 +1,3 @@
 export { ModCard } from "./ModCard";
 export { ModVisualizerModal } from "./ModVisualizerModal";
 export { UserProfileModal } from "./UserProfileModal";
-export {
-  activeDownloads,
-  availableUpdates,
-  engines,
-  featuredMod,
-  installedMods,
-  recentlyUpdatedMods,
-  recommendedMods,
-  trendingMods,
-} from "./mockData";
